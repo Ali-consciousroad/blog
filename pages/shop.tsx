a6889e1922e5bd539a1ed7e2082af833630f26c4
Ali-consciousroad <ali_dindar@live.be>
2025-05-27 19:15:16 +0300

    feat(shop): add coming soon placeholder for shop section

---
+++ pages/shop.tsx
@@ -0,0 +1,11 @@
+import Container from "../components/container";
+
+export default function Shop() {
+  return (
+    <Container>
+      <div className="flex flex-col items-center justify-center min-h-[60vh]">
+        <p className="text-2xl text-gray-500">Coming Soon</p>
+      </div>
+    </Container>
+  );
+} 
